927ca36f56844b8380c71200c07aeac090fd10c1
wangcheng <995678742@qq.com>
2022-04-14 13:24:41 +0800

    1

---
+++ B_Category/Source/Target/CTMediator+B.h
@@ -0,0 +1,16 @@
+//
+//  CTMediator+B.h
+//  B_Category
+//
+//  Created by WangC on 2022/4/12.
+//
+
+#import <CTMediator/CTMediator.h>
+#import <UIKit/UIKit.h>
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CTMediator (B)
+- (UIViewController *)B_viewControllerWithContentText:(NSString *)contentText;
+@end
+
+NS_ASSUME_NONNULL_END
